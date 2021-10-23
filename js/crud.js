1b74e49fb147c29290cc34a1198d86386a9c0e25
acarbajal-zigla <acarbajal@ziglaconsultores.com>
2021-10-23 14:59:35 -0300

    creado CRUD

--- js/crud.js
+++ js/crud.js
@@ -1,3 +1,9 @@
-const create = (Persona){
-    nombre = document.getElementById('input-nombrePersona')
+const create = (Persona) => {
+    const nombre = document.getElementById('input-nombre').value;
+    const apellido = document.getElementById('input-apellido').value;
+    const montoInicial = document.getElementById('input-montoInicial').value;
+    const gastosMensuales = document.getElementById('input-gastosMensuales').value;
+
+    localStorage.create()
 }
+
