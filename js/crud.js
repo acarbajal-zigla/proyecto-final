5537ecb72cc2145430697a0179d2ef4e36b629ae
acarbajal-zigla <acarbajal@ziglaconsultores.com>
2021-11-03 22:00:53 -0300

    entregable ordenado

--- js/crud.js
+++ js/crud.js
@@ -21,33 +21,6 @@ let personas = JSON.parse(localStorage.getItem('personas')) || [];
 
 renderPersonasTable();
 
-function renderPersonasTable() {
-    tablaPersonas.innerHTML = "";
-    let tableBody = document.createElement("tbody");
-    let header = ['Nombre', 'Apellido', 'Presupuesto inicial', 'Gastos', 'Presupuesto para el resto del mes', 'Monto por día (resto del mes)'];
-    let filaTitulos = document.createElement("tr");
-
-    for (let element of header) {
-        let celda = document.createElement("td");
-        celda.appendChild(document.createTextNode(element));
-        filaTitulos.appendChild(celda);
-    }
-    tableBody.appendChild(filaTitulos);
-
-    for (let persona of personas) {
-        let fila = document.createElement("tr");
-        let presupuesto = new Presupuesto(persona.presupuesto.montoInicial, persona.presupuesto.gastos)
-
-        for (element of [persona.nombre, persona.apellido, presupuesto.getMontoInicial(), presupuesto.getGastos(), presupuesto.getRestoNeto(), presupuesto.getRestoDiario()]) {
-            let celda = document.createElement("td");
-            celda.appendChild(document.createTextNode(element));
-            fila.appendChild(celda);
-        }
-        tableBody.appendChild(fila);
-    }
-    tablaPersonas.appendChild(tableBody);
-}
-
 function getAll() {
     return JSON.parse(localStorage.getItem("personas"));
 }
